e3e2d80739255d7ecee2ef2602354c275662f3ab
PyTom <107923033+PyyTom@users.noreply.github.com>
2024-05-02 11:04:52 +0200

    Update pwgen.py

--- pwgen.py
+++ pwgen.py
@@ -2,12 +2,15 @@ import string,random,flet as fl
 def main(page:fl.Page):
     def xit(e):
         page.window_destroy()
+    def copy(e):
+        copied=generated.value
+        print(copied)
     def generate(e):
         generated.value=''.join(random.choice(string.ascii_letters+string.digits+string.punctuation) for i in range(8))
         page.update()
     generated=fl.TextField(label='GENERATED',disabled=True)
     page.add(fl.Text('RANDOM PASSWORD GENERATOR'),
              fl.ElevatedButton('GENERATE',on_click=generate),
-             generated,
+             fl.Row(controls=[generated,fl.IconButton(icon=fl.icons.CONTENT_COPY,on_click=copy)]),
              fl.ElevatedButton('EXIT',on_click=xit))
 fl.app(target=main)
