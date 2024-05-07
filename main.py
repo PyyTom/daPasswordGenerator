9c8b4fe8eb20aaebb312131780a0586347557ef2
PyyTom <tommaso_latorre@hotmail.com>
2024-05-07 16:52:16 +0200

    just updated

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import string,random,flet as fl
+import pyperclip,string,random,flet as fl
 def main(page:fl.Page):
     def xit(e):
         page.window_destroy()
     def copy(e):
-        copied=generated.value
-        print(copied)
+        pyperclip.copy(generated.value)
+        copied.value='COPIED'
+        copied.update()
     def generate(e):
+        copied.value=''
+        copied.update()
         generated.value=''.join(random.choice(string.ascii_letters+string.digits+string.punctuation) for i in range(8))
         page.update()
+    page.window_width=500
+    page.window_height=220
+    page.theme_mode=fl.ThemeMode.LIGHT
+    copied=fl.Text()
     generated=fl.TextField(label='GENERATED',disabled=True)
     page.add(fl.Text('RANDOM PASSWORD GENERATOR'),
              fl.ElevatedButton('GENERATE',on_click=generate),
-             fl.Row(controls=[generated,fl.IconButton(icon=fl.icons.CONTENT_COPY,on_click=copy)]),
+             fl.Row(controls=[generated,fl.IconButton(icon=fl.icons.CONTENT_COPY,on_click=copy),copied]),
              fl.ElevatedButton('EXIT',on_click=xit))
 fl.app(target=main)
